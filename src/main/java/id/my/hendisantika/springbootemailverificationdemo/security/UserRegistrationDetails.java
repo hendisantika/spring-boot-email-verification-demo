c2bc6b95e6d33ed22808b5de0f69313772d658ef
hendisantika <hendisantika@yahoo.co.id>
2024-02-27 08:01:40 +0700

    Add more fields in UserRegistrationDetails 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootemailverificationdemo/security/UserRegistrationDetails.java
+++ src/main/java/id/my/hendisantika/springbootemailverificationdemo/security/UserRegistrationDetails.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -36,5 +37,40 @@ public class UserRegistrationDetails implements UserDetails {
                         .split(","))
                 .map(SimpleGrantedAuthority::new)
                 .collect(Collectors.toList());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isEnabled;
     }
 }
