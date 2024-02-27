e311993b0e35f914214169c77004b62ae8c6095c
hendisantika <hendisantika@yahoo.co.id>
2024-02-27 07:51:49 +0700

    Add RegistrationExceptionHandler 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootemailverificationdemo/exception/RegistrationExceptionHandler.java
@@ -0,0 +1,42 @@
+package id.my.hendisantika.springbootemailverificationdemo.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-email-verification-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2/27/24
+ * Time: 07:51
+ * To change this template use File | Settings | File Templates.
+ */
+@RestControllerAdvice
+public class RegistrationExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult()
+                .getFieldErrors()
+                .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+        return errors;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public Map<String, String> userNotFound(UserAlreadyExistsException ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", ex.getMessage());
+        return error;
+    }
+}
