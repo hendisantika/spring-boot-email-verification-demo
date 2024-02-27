5543928d49fc318a9fab7b7551e46c0bdf52e858
hendisantika <hendisantika@yahoo.co.id>
2024-02-27 07:41:03 +0700

    Create starter project 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootemailverificationdemo/SpringBootEmailVerificationDemoApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootemailverificationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEmailVerificationDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootEmailVerificationDemoApplication.class, args);
+    }
+
+}
