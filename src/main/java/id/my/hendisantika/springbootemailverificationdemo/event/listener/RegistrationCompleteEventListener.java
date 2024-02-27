505c180a7e24af9bfbff37c4046cb9bb319a378a
hendisantika <hendisantika@yahoo.co.id>
2024-02-27 07:53:59 +0700

    Add onApplicationEvent in RegistrationCompleteEventListener 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootemailverificationdemo/event/listener/RegistrationCompleteEventListener.java
+++ src/main/java/id/my/hendisantika/springbootemailverificationdemo/event/listener/RegistrationCompleteEventListener.java
@@ -3,11 +3,15 @@ package id.my.hendisantika.springbootemailverificationdemo.event.listener;
 import id.my.hendisantika.springbootemailverificationdemo.entity.User;
 import id.my.hendisantika.springbootemailverificationdemo.event.RegistrationCompleteEvent;
 import id.my.hendisantika.springbootemailverificationdemo.service.UserService;
+import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
+
+import java.io.UnsupportedEncodingException;
+import java.util.UUID;
 
 /**
  * Created by IntelliJ IDEA.
@@ -27,4 +31,23 @@ public class RegistrationCompleteEventListener implements ApplicationListener<Re
 
     private final JavaMailSender mailSender;
     private User theUser;
+
+    @Override
+    public void onApplicationEvent(RegistrationCompleteEvent event) {
+        // 1. Get the newly registered user
+        theUser = event.getUser();
+        //2. Create a verification token for the user
+        String verificationToken = UUID.randomUUID().toString();
+        //3. Save the verification token for the user
+        userService.saveUserVerificationToken(theUser, verificationToken);
+        //4 Build the verification url to be sent to the user
+        String url = event.getApplicationUrl() + "/register/verifyEmail?token=" + verificationToken;
+        //5. Send the email.
+        try {
+            sendVerificationEmail(url);
+        } catch (MessagingException | UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+        log.info("Click the link to verify your registration :  {}", url);
+    }
 }
