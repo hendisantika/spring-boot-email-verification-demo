26bf57502108893a0ec3cbce0218d7c3bf158e8c
hendisantika <hendisantika@yahoo.co.id>
2024-02-27 08:02:03 +0700

    Add UserRegistrationDetailsService 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootemailverificationdemo/security/UserRegistrationDetailsService.java
@@ -0,0 +1,31 @@
+package id.my.hendisantika.springbootemailverificationdemo.security;
+
+import id.my.hendisantika.springbootemailverificationdemo.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-email-verification-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2/27/24
+ * Time: 08:01
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@RequiredArgsConstructor
+public class UserRegistrationDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .map(UserRegistrationDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
