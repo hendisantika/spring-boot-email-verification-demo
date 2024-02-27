60986a993cdb40bbe26fc1d49fd82fe7e7ca3940
hendisantika <hendisantika@yahoo.co.id>
2024-02-27 07:44:17 +0700

    Add User Entity 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootemailverificationdemo/entity/User.java
@@ -0,0 +1,39 @@
+package id.my.hendisantika.springbootemailverificationdemo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.NaturalId;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-email-verification-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2/27/24
+ * Time: 07:43
+ * To change this template use File | Settings | File Templates.
+ */
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    @NaturalId(mutable = true)
+    private String email;
+    private String password;
+    private String role;
+    private boolean isEnabled = false;
+}
