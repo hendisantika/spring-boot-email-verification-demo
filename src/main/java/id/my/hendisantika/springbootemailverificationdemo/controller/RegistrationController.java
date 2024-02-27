9cadf1149caeb823069b39fc6bb053afe22e8c78
hendisantika <hendisantika@yahoo.co.id>
2024-02-27 07:59:02 +0700

    Add registerUser in RegistrationController 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootemailverificationdemo/controller/RegistrationController.java
+++ src/main/java/id/my/hendisantika/springbootemailverificationdemo/controller/RegistrationController.java
@@ -1,9 +1,15 @@
 package id.my.hendisantika.springbootemailverificationdemo.controller;
 
+import id.my.hendisantika.springbootemailverificationdemo.dto.RegistrationRequest;
+import id.my.hendisantika.springbootemailverificationdemo.entity.User;
+import id.my.hendisantika.springbootemailverificationdemo.event.RegistrationCompleteEvent;
 import id.my.hendisantika.springbootemailverificationdemo.repository.VerificationTokenRepository;
 import id.my.hendisantika.springbootemailverificationdemo.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,4 +31,11 @@ public class RegistrationController {
     private final UserService userService;
     private final ApplicationEventPublisher publisher;
     private final VerificationTokenRepository tokenRepository;
+
+    @PostMapping
+    public String registerUser(@RequestBody RegistrationRequest registrationRequest, final HttpServletRequest request) {
+        User user = userService.registerUser(registrationRequest);
+        publisher.publishEvent(new RegistrationCompleteEvent(user, applicationUrl(request)));
+        return "Success!  Please, check your email for to complete your registration";
+    }
 }
